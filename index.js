d91dc8993ba1396998074d79b4dba4f87f76224e
Dennis Hagemeier <hello@d-hagemeier.com>
2022-04-01 11:34:49 +0200

    Added debug mode to log more information

--- index.js
+++ index.js
@@ -5,17 +5,37 @@ const { minify } = require('html-minifier-terser');
 module.exports = {
   onPostBuild: async ({ inputs, constants, utils }) => {
     const files = glob.sync(constants.PUBLISH_DIR + '/**/*.html');
-    console.log(`Found ${files.length} files...`);
+    const debug = inputs.debug;
+    let i = 0;
+
+    if (debug) { 
+      console.log("Plugin inputs:", inputs);
+      console.log(`Found ${files.length} files...`);
+    };
 
     await Promise.all(files.map(async (file) => {
       try {
         const content = fs.readFileSync(file, 'utf-8');
         const minified = await minify(content, inputs.minify_options);
         const out = fs.writeFileSync(file, minified);
-        console.log(`✅ Minified ${file}`);
+
+        if (debug) {
+          const details = {
+            "Old size in characters": content.length,
+            "Old size in byte": Buffer.byteLength(content, 'utf8'),
+            "New size in characters": minified.length,
+            "New size in byte": Buffer.byteLength(minified, 'utf8'),
+            "Difference": Buffer.byteLength(minified, 'utf8') - Buffer.byteLength(content, 'utf8') + " byte"
+          };
+
+          console.log(`✅ Minified ${file}`, details) 
+        };
+        i++;
       } catch(err) {
-        console.log(`❌ Failed minimization of ${err}`);
-      }    
+        if (debug) { console.log(`❌ Failed minimization of ${file}, error: ${err}`) };
+      }
     }))
+
+    console.log(`Minified ${i} files`)
   }
 }
